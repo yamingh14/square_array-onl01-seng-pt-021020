52a135b7c1ec19b290cca9046ea757d114c295b4
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-17 01:45:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  array = [1, 2, 3]
-  array2 =[]
+  # array = [1, 2, 3]
+  array2 = []
   array.each do |element|
   array1 = element ** 2
   array2 << array1
   end
   array2
 end
-square_array("array")
+# square_array("array")
